79acc1e3e9e30f6ef18057cd400a0a7d9af5f52a
rdbckp <ardibackup@gmail.com>
2025-12-15 18:29:59 +0700

    Update build_kernel.sh
    Signed-off-by: rdbckp <ardibackup@gmail.com>

--- build_kernel.sh
+++ build_kernel.sh
@@ -5,22 +5,20 @@ set -e -x
 # custom toolchain preparation
 cd ${GITHUB_WORKSPACE}
 ls
-export PATH="${PWD}/toolchain2/clang/bin:${PWD}/toolchain2/gcc/bin:${PATH}"
+export PATH="${PWD}/toolchain2/gcc/bin:${PATH}"
 
 # export PATH="${PWD}/toolchain/clang/bin:${PWD}/toolchain/gcc/bin:${PATH}"
 export ARCH=arm
-export CC=clang
-export HOSTCC=clang
-export CLANG_TRIPLE=arm-linux-gnueabi-
 export CROSS_COMPILE=arm-linux-androideabi-
 export KCFLAGS=-w
 export CONFIG_SECTION_MISMATCH_WARN_ONLY=y
 rm -rf out
 mkdir -p out
-make O=out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y ARCH=arm CC=clang HOSTCC=clang CLANG_TRIPLE=arm-linux-gnueabi- CROSS_COMPILE=arm-linux-androidkernel- clean
-make O=out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y ARCH=arm CC=clang HOSTCC=clang CLANG_TRIPLE=arm-linux-gnueabi- CROSS_COMPILE=arm-linux-androidkernel- mrproper
-make O=out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y ARCH=arm CC=clang HOSTCC=clang CLANG_TRIPLE=arm-linux-gnueabi- CROSS_COMPILE=arm-linux-androidkernel- s5pv210_defconfig
-make O=out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y ARCH=arm CC=clang HOSTCC=clang CLANG_TRIPLE=arm-linux-gnueabi- CROSS_COMPILE=arm-linux-androidkernel- -j16
+
+make O=out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y ARCH=arm CROSS_COMPILE=arm-linux-androidkernel- defconfig
+make O=out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y ARCH=arm CROSS_COMPILE=arm-linux-androidkernel- KCONFIG_ALLCONFIG=.config allnoconfig
+make O=out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y ARCH=arm CROSS_COMPILE=arm-linux-androidkernel- -j$(nproc --all)
+make O=out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y ARCH=arm CROSS_COMPILE=arm-linux-androidkernel- modules_install
 
 cp out/arch/arm/boot/zImage ${PWD}/zImage
 mv zImage boot.img-kernel
